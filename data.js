6c588e1cd2ace4c2a902d71beb7ff0b10dce2937
Pavlyuchenko <michaelg.pavlicek@gmail.com>
2021-08-20 17:37:52 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,114 @@
+let tree = {
+	title: "Main continents",
+	children: [
+		{
+			title: "Europe",
+			description: `The oldest monarchy is located in Europe. <br> 
+			<img src='https://upload.wikimedia.org/wikipedia/commons/b/b9/Europe_countries_map_de_2.png' alt='img' height='200' />`,
+			children: [
+				{
+					title: "Germany",
+					children: [
+						{
+							title: "Berlin",
+						},
+						{
+							title: "Leipzig",
+						},
+					],
+				},
+				{
+					title: "Great Britain",
+					children: [
+						{
+							title: "London",
+						},
+						{
+							title: "Manchester",
+						},
+					],
+				},
+				{
+					title: "France",
+					children: [
+						{
+							title: "Paris",
+						},
+						{
+							title: "Marseille",
+						},
+						{
+							title: "Lyon",
+						},
+					],
+				},
+				{
+					title: "Spain",
+					children: [
+						{
+							title: "Madrid",
+						},
+						{
+							title: "Barcelona",
+						},
+					],
+				},
+			],
+		},
+		{
+			title: "North America",
+			description: `North America was named after an Italian Explorer <br> 
+				<img src='https://res.cloudinary.com/fen-learning/image/upload/c_limit,w_510,h_383/infopls_images/images/mapnorthamerica.gif' alt='img' height='200' />`,
+			children: [
+				{
+					title: "USA",
+				},
+				{
+					title: "Canada",
+				},
+				{
+					title: "Mexico",
+				},
+			],
+		},
+		{
+			title: "South America",
+			description: `South America has nothing to do with North America. They are both completely different continents. <br> 
+			<img src='https://upload.wikimedia.org/wikipedia/commons/d/d0/Map_of_South_America_%28nl%29.png' alt='img' height='200' />`,
+			children: [
+				{
+					title: "Brazil",
+				},
+				{
+					title: "Argentina",
+				},
+			],
+		},
+		{
+			title: "Asia",
+			description: `Asia is the most populated continent of them all. <br> 
+			<img src='https://www.beltandroad.news/wp-content/uploads/2020/05/Asia_Map-scaled.jpg' alt='img' height='200' />`,
+			children: [
+				{
+					title: "China",
+				},
+				{
+					title: "India",
+				},
+			],
+		},
+		{
+			title: "Africa",
+			description: `Africa is the poorest continent on Earth.<br> 
+			<img src='https://geology.com/world/africa-map.gif' alt='img' height='200' />`,
+			children: [
+				{
+					title: "Algeria",
+				},
+				{
+					title: "Congo",
+				},
+			],
+		},
+	],
+};
